45c7a872fd1eb6a1edae4054bb82a388deada1c0
Mariela-t <tinoco.mariela01@gmail.com>
2023-06-12 10:44:06 -0700

    used reactstrap instead of bootstrap, still need to do the same to the profile page

--- src/components/pages/EditProfile.js
+++ src/components/pages/EditProfile.js
@@ -1,97 +1,124 @@
 import React from "react";
-import "./css/EditProfile.css";
+import {
+  Container,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+} from "reactstrap";
+// import "./css/EditProfile.css";
 
 function EditProfile() {
   return (
     <>
-      <img src="..." className="rounded float-left" alt="..."></img>
-      <div className="editProfilePage">
+      <img src="..." className="" alt="..." />
+
+      <Container>
         <h3>Pet Information</h3>
-        <form>
-          <div className="row">
-            <div className="col-md-6">
-              <label htmlFor="petName" className="form-label">
-                Pet Name
-              </label>
-              <input type="text" className="form-control" id="petName" />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="status" className="form-label">
-                Lost or found?
-              </label>
-              <input type="text" className="form-control" id="status" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label htmlFor="animalType" className="form-label">
-                Animal type
-              </label>
-              <input type="" className="form-control" id="animalType" />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="breed" className="form-label">
-                Breed
-              </label>
-              <input type="text" className="form-control" id="breed" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label htmlFor="color" className="form-label">
-                Color
-              </label>
-              <input type="text" className="form-control" id="color" />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="gender" className="form-label">
-                Gender
-              </label>
-              <input type="text" className="form-control" id="gender" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <label htmlFor="location" className="form-label">
-                Location (Zip Code)
-              </label>
-              <input type="number" className="form-control" id="location" />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="dateFound" className="form-label">
-                Date found
-              </label>
-              <input type="date" className="form-control" id="dateFound" />
-            </div>
-          </div>
-          <h3>Contact Information</h3>
-          <div className="row">
-            <div className="col-md-6">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="userEmail"
-                aria-describedby="emailHelp"
-              />
-              <div id="emailHelp" className="form-text">
-                We'll never share your email with anyone else.
-              </div>
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="userPhoneNumber" className="form-label">
-                Phone Number
-              </label>
-              <input type="tel" className="form-control" id="userPhoneNumber" />
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Update
-          </button>
-        </form>
-      </div>
+
+        <Form>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="petName" className="form-label">
+                  Pet Name
+                </Label>
+                <Input type="text" className="form-control" id="petName" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="status" className="form-label">
+                  Lost or found?
+                </Label>
+                <Input type="text" className="form-control" id="status" />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="animalType" className="form-label">
+                  Animal type
+                </Label>
+                <Input type="" className="form-control" id="animalType" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="breed" className="form-label">
+                  Breed
+                </Label>
+                <Input type="text" className="form-control" id="breed" />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="color" className="form-label">
+                  Color
+                </Label>
+                <Input type="text" className="form-control" id="color" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="gender" className="form-label">
+                  Gender
+                </Label>
+                <Input type="text" className="form-control" id="gender" />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="location" className="form-label">
+                  Location (Zip Code)
+                </Label>
+                <Input type="text" className="form-control" id="location" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="dateFound" className="form-label">
+                  Date found
+                </Label>
+                <Input type="date" className="form-control" id="dateFound" />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
+        <h3>Contact Information</h3>
+        <Form>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="userEmail">Email</Label>
+                <Input type="email" name="email" id="userEmail" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label htmlFor="userPhoneNumber" className="form-label">
+                  Phone Number
+                </Label>
+                <Input
+                  type="tel"
+                  className="form-control"
+                  id="userPhoneNumber"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Button>Update</Button>
+        </Form>
+      </Container>
     </>
   );
 }
