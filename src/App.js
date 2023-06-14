195836f61cb453a2ca7e6336b5faa6f234cbbe35
Mariela-t <tinoco.mariela01@gmail.com>
2023-06-14 15:21:04 -0700

    added react strap to profile page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import DashBoard from "./components/pages/Dashboard";
 
-import LostOrFoundChoice from './components/pages/LostOrFoundChoice'
+import LostOrFoundChoice from "./components/pages/LostOrFoundChoice";
 import AddFoundPet from "./components/pages/AddFoundPet";
 import AddLostPet from "./components/pages/AddLostPet";
 import RegisterPage from "./components/pages/RegisterPage";
@@ -16,42 +16,16 @@ import EditProfile from "./components/pages/EditProfile";
 // Defines the routes for the application
 function App() {
   return (
-    
     <Routes>
-      <Route
-        path="/login"
-        element={<LoginPage />}
-      />
-      <Route
-        path="/dashboard"
-        element={<DashBoard/>}
-      />
-      <Route
-      path="/selectlostorfound"
-      element={<LostOrFoundChoice/>}
-      />
-      <Route
-      path="/addfoundpet"
-      element={<AddFoundPet/>}
-      />
-        <Route
-      path="/addlostpet"
-      element={<AddLostPet/>}
-      />
-      <Route
-        path="/profile"
-        element={<ProfilePage />}
-      />
-      <Route
-        path="/profile-edit"
-        element={<EditProfile />}
-      />
-      <Route
-        path="/register"
-        element={<RegisterPage />}
-      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/selectlostorfound" element={<LostOrFoundChoice />} />
+      <Route path="/addfoundpet" element={<AddFoundPet />} />
+      <Route path="/addlostpet" element={<AddLostPet />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/profile-edit" element={<EditProfile />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
-
   );
 }
 
