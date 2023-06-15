7bf2189ab41811adfb225c3fb6be887cb8082565
trca831 <103814052+trca831@users.noreply.github.com>
2023-06-14 18:14:14 -0700

    Finished implementing card component from react-strap to dashboard page

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 
 const petList = [
     {
         id: 1,
         title: "Charlie",
         city: "Fremont, California",
+        status: "Lost",
         dateMissing: "2/29/23",
         imgUrl:"http://placekitten.com/300/200",
     },
@@ -12,6 +14,7 @@ const petList = [
         id: 2,
         title: "Willow",
         city: "San Jose, California",
+        status: "Found",
         dateMissing: "1/04/23",
         imgUrl:"http://placekitten.com/400/200",
     },
@@ -19,6 +22,7 @@ const petList = [
         id:3,
         title: "Luna",
         city: "Berkeley, California",
+        status: "Found",
         dateMissing: "12/19/22",
         imgUrl:"http://placekitten.com/400/300",
     },
@@ -26,6 +30,7 @@ const petList = [
         id:4,
         title: "Kilo",
         city: "San Francisco, California",
+        status: "Lost",
         dateMissing: "1/4/23",
         imgUrl:"http://placekitten.com/400/200",
     }
@@ -33,18 +38,50 @@ const petList = [
 
 function Dashboard() {
     return (
-       <div>
+
+<>
         <button type="button"> Show Filters </button>
         <button type="button"> Add Pet </button>
         <ul>
         {petList.map(function(item){
             return (
-            <li key={item.id}> <br/> {item.imgUrl} <br/> {item.title} <br/> {item.city} <br/> Missing since: {item.dateMissing}  </li>
-          
-            )})}
-        </ul>
-        
-       </div>
+            <li key={item.id} style={{listStyleType: "none"}}> <br/> 
+    
+        <Card
+            style={{
+                width: '18rem'
+                }}
+                >
+                <img
+                    alt="Sample"
+                    src={item.imgUrl}
+                />
+                <CardBody>
+                    <div style={{display:"flex", flexDirection:"row"}}>
+                        <CardTitle tag="h5">
+                        {item.title}
+                        </CardTitle>
+                        <br/>
+                    <Button>
+                    {item.status}
+                    </Button>
+                    </div>
+                        <CardSubtitle
+                        className="mb-2 text-muted"
+                        tag="h6"
+                        >
+                        {item.city}
+                        </CardSubtitle>
+                    <CardText>
+                    Missing since: {item.dateMissing}
+                    </CardText>
+                </CardBody>
+        </Card>
+                </li>
+                )})}
+                </ul>
+       </>
+
     )      
     
 }
