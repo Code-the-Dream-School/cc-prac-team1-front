d6d37fe40ab1577031e5f48f9c4b323afee082ac
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-06-18 21:25:54 -0500

    added icons pending stypling

--- src/components/pages/LostOrFoundChoice.js
+++ src/components/pages/LostOrFoundChoice.js
@@ -1,16 +1,18 @@
-import { Container, Button } from "reactstrap";
+import { Container } from "reactstrap";
 import { useNavigate } from "react-router-dom";
 import "./css/LostOrFoundChoice.css";
+import FoundIcon from  "../../creative-assets/found-icon.js"
+import LostIcon from "../../creative-assets/lost-icon"; 
 
 const LostOrFound = () => {
   const goToAddPet = useNavigate();
 
   return (
-    <Container className="center-container">
+    <Container className="center-container" >
       <p>Report a Lost of Found Pet</p>
       <div className="center-button">
-        <Button onClick={() => goToAddPet("/addlostpet")}>Lost Pet</Button>
-        <Button onClick={() => goToAddPet("/addfoundpet")}>Found Pet</Button>
+      <LostIcon navigation ={goToAddPet}/>
+       <FoundIcon navigation ={goToAddPet} />
       </div>
     </Container>
   );
--- src/components/pages/map.js
+++ src/components/pages/map.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/Map.css";
+// import React, { useEffect } from "react";
+// import L from "leaflet";
+// import "leaflet/dist/leaflet.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./css/Map.css";
 
-const MapComponent = () => {
-  useEffect(() => {
-    const location = [29.4241, -98.4936]; // San Antonio, Texas coordinates. These will be provided by the user. For now, they are hardcoded.
+// const MapComponent = () => {
+//   useEffect(() => {
+//     const location = [29.4241, -98.4936]; // San Antonio, Texas coordinates. These will be provided by the user. For now, they are hardcoded.
 
-    const map = L.map("map").setView(location, 13);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-    }).addTo(map);
-  }, []);
+//     const map = L.map("map").setView(location, 13);
+//     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//       attribution:
+//         'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+//     }).addTo(map);
+//   }, []);
 
-  return (
-    <div style={{ position: "relative" }}>
-      <div className="map-button-container">
-        <button className="map-button-primary">Button 1</button>
-        <button className="map-button-secondary">Button 2</button>
-      </div>
-      <div
-        id="map"
-        style={{ height: "100vh", width: "100vw" }}
-      ></div>
-    </div>
-  );
-};
+//   return (
+//     <div style={{ position: "relative" }}>
+//       <div className="map-button-container">
+//         <button className="map-button-primary">Button 1</button>
+//         <button className="map-button-secondary">Button 2</button>
+//       </div>
+//       <div
+//         id="map"
+//         style={{ height: "100vh", width: "100vw" }}
+//       ></div>
+//     </div>
+//   );
+// };
 
-export default MapComponent;
+// export default MapComponent;
