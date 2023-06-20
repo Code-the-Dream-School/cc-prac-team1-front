71e9c658d169e169cfd095291571ccb415285580
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-06-20 18:39:19 -0500

    Integrates geocoding

--- src/App.js
+++ src/App.js
@@ -6,60 +6,60 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import DashBoard from "./components/pages/Dashboard";
-import MapComponent from "./components/pages/map";
+import MapComponent from "./components/pages/Map";
 import LostOrFoundChoice from "./components/pages/LostOrFoundChoice";
 import AddFoundPet from "./components/pages/AddFoundPet";
 import AddLostPet from "./components/pages/AddLostPet";
 import ProfilePage from "./components/pages/ProfilePage";
 import EditProfile from "./components/pages/EditProfile";
-import NavBar from "./components/elements/NavBar"
+import NavBar from "./components/elements/NavBar";
 
 // Defines the routes for the application
 function App() {
   const location = useLocation();
-  const navBarPaths = ["/", "/about", "/contact", "/dashboard", "/profile"]
-  const showNabBar = navBarPaths.includes(location.pathname)
+  const navBarPaths = ["/", "/about", "/contact", "/dashboard", "/profile"];
+  const showNabBar = navBarPaths.includes(location.pathname);
   return (
     <div>
-      {showNabBar && <NavBar/>}
-    <Routes>
-         <Route
-        path="/login"
-        element={<LoginPage />}
-      />
-      <Route
-        path="/register"
-        element={<RegisterPage />}
-      />
-      <Route
-        path="/dashboard"
-        element={<DashBoard />}
-      />
-      <Route
-        path="/map"
-        element={<MapComponent />}
-      />
-      <Route
-        path="/selectlostorfound"
-        element={<LostOrFoundChoice />}
-      />
-      <Route
-        path="/addfoundpet"
-        element={<AddFoundPet />}
-      />
-      <Route
-        path="/addlostpet"
-        element={<AddLostPet />}
-      />
-      <Route
-        path="/profile"
-        element={<ProfilePage />}
-      />
-      <Route
-        path="/profile-edit"
-        element={<EditProfile />}
-      />
-    </Routes>
+      {showNabBar && <NavBar />}
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+        <Route
+          path="/dashboard"
+          element={<DashBoard />}
+        />
+        <Route
+          path="/map"
+          element={<MapComponent />}
+        />
+        <Route
+          path="/selectlostorfound"
+          element={<LostOrFoundChoice />}
+        />
+        <Route
+          path="/addfoundpet"
+          element={<AddFoundPet />}
+        />
+        <Route
+          path="/addlostpet"
+          element={<AddLostPet />}
+        />
+        <Route
+          path="/profile"
+          element={<ProfilePage />}
+        />
+        <Route
+          path="/profile-edit"
+          element={<EditProfile />}
+        />
+      </Routes>
     </div>
   );
 }
