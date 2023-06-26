c7ce5ed5cf18176cdb2c817b3cb0fa39df012e92
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-06-26 14:06:34 -0500

    changes made to merge

--- src/components/pages/AddFoundPet.js
+++ src/components/pages/AddFoundPet.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const AddFoundPet = () => {
   const initialValues = {
-    image: null,
+    image: "",
     petName: "",
     animalType: "",
     breed: "",
--- src/components/pages/AddLostPet.js
+++ src/components/pages/AddLostPet.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const AddLostPet = () => {
   const initialValues = {
-    image: null,
+    image: "",
     petName: "",
     animalType: "",
     breed: "",
