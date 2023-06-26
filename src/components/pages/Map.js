4842aa3f8d44e2d31e1638c74575389acbe986b1
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-06-26 14:27:00 -0500

    Adds comments to the code

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -7,42 +7,49 @@ import "./css/Map.css";
 import axios from "axios";
 
 const MapComponent = () => {
-  const [userProvidedZipCode, setUserProvidedZipCode] = useState("");
-  const [showPrompt, setShowPrompt] = useState(true);
+  const [userProvidedZipCode, setUserProvidedZipCode] = useState(""); // Initializes state variables for the user-provided ZIP code
+  const [showPrompt, setShowPrompt] = useState(true); // Initializes state variables to control the visibility of the prompt
+
   const mapRef = useRef(null); // Ref to store the map instance
 
-  // Function to geocode a given ZIP code and return latitude and longitude
+  // Defines an asynchronous function to geocode a ZIP code using MapQuest API
   const geocodeZipCode = async (zipcode) => {
     try {
+      // Makes an HTTP GET request to the MapQuest API with the provided ZIP code
       const response = await axios.get(
         `https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_KEY}&location=${zipcode}`
       );
+
+      // Extracts the latitude and longitude from the response data
       const { lat, lng } = response.data.results[0].locations[0].latLng;
+
+      // Returns an object containing the latitude and longitude
       return { lat, lng };
     } catch (error) {
+      // Throws an error if there's an issue with geocoding the ZIP code
       throw new Error("Error geocoding ZIP code:", error);
     }
   };
 
   // Function to add a marker to the map with the provided latitude, longitude, and pet information
   const addMarkerToMap = (lat, lng, petInfo) => {
-    const randomOffset = Math.random() * 0.018;
+    const randomOffset = Math.random() * 0.018; // Calculates a random offset for the marker position
 
     const markerIcon = L.divIcon({
       html: `<i class="bi bi-geo-alt-fill" style="color: ${
         petInfo.petSituation === "found" ? "#2793E2" : "#FD678D"
-      };"></i>`,
-      iconSize: [25, 25],
+      };"></i>`, // Creates a marker icon with a color based on the pet situation (found or missing)
+      iconSize: [25, 25], // Sets the size of the marker icon
     });
 
     const marker = L.marker([lat + randomOffset, lng + randomOffset], {
       icon: markerIcon,
-    }).addTo(mapRef.current);
+    }).addTo(mapRef.current); // Adds a marker to the map at the specified latitude and longitude with the marker icon
 
     const missingFoundText =
-      petInfo.petSituation === "found" ? "Found on" : "Missing since";
+      petInfo.petSituation === "found" ? "Found on" : "Missing since"; // Determines the text based on the pet situation
 
-    // Format the phone number
+    // Formats the phone number
     const formattedPhoneNumber = `(${petInfo.contact.phone.slice(
       0,
       3
@@ -64,47 +71,49 @@ const MapComponent = () => {
       </div>
     `;
 
-    marker.bindPopup(popupContent);
+    marker.bindPopup(popupContent); // Binds the popup content to the marker
   };
 
-  // Handle form submission for ZIP code prompt
+  // Handles form submission for ZIP code prompt
   const handlePromptSubmit = (e) => {
-    e.preventDefault();
-    const zipCodeInput = document.getElementById("zipCodeInput");
-    setUserProvidedZipCode(zipCodeInput.value);
-    setShowPrompt(false);
+    e.preventDefault(); // Prevents default form submission behavior
+    const zipCodeInput = document.getElementById("zipCodeInput"); // Gets the ZIP code input element
+    setUserProvidedZipCode(zipCodeInput.value); // Sets the user provided ZIP code
+    setShowPrompt(false); // Hides the prompt after form submission
   };
 
-  // Initialize the map instance
+  // Initializes the map instance
   useEffect(() => {
     mapRef.current = L.map("map").setView([0, 0], 13);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      // Adds the tile layer from OpenStreetMap
       attribution:
         'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-    }).addTo(mapRef.current);
+    }).addTo(mapRef.current); // Adds the tile layer to the map instance
 
     return () => {
-      mapRef.current.remove(); // Remove the map when the component unmounts
+      mapRef.current.remove(); // Removes the map when the component unmounts
     };
   }, []);
 
-  // Fetch pet data and geocode ZIP code when user provides a ZIP code
+  // Fetches pet data and geocode ZIP code when user provides a ZIP code
   useEffect(() => {
     if (userProvidedZipCode) {
-      geocodeZipCode(userProvidedZipCode)
+      geocodeZipCode(userProvidedZipCode) // Geocodes the user provided ZIP code
         .then(({ lat, lng }) => {
-          mapRef.current.setView([lat, lng], 13); // Update the map view
+          mapRef.current.setView([lat, lng], 13); // Sets the map view to the geocoded coordinates
 
           axios
-            .get("http://localhost:5005/api/v1/pets")
+            .get("http://localhost:5005/api/v1/pets") // Fetches pet data from the API
             .then((response) => {
               const fetchedPets = response.data;
 
               fetchedPets.forEach(async (pet) => {
+                // Iterates over the fetched pets
                 try {
-                  const { lat, lng } = await geocodeZipCode(pet.petLocation);
-                  addMarkerToMap(lat, lng, pet);
+                  const { lat, lng } = await geocodeZipCode(pet.petLocation); // Geocodes the pet's ZIP code
+                  addMarkerToMap(lat, lng, pet); // Adds a marker to the map for the pet
                 } catch (error) {
                   console.error("Error geocoding pet's ZIP code:", error);
                 }
@@ -120,7 +129,7 @@ const MapComponent = () => {
     }
   }, [userProvidedZipCode]);
 
-  // Show prompt if ZIP code is not provided
+  // Shows prompt if ZIP code is not provided
   useEffect(() => {
     if (!userProvidedZipCode) {
       setShowPrompt(true);
